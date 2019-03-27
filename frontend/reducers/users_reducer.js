e5d69036434621e3dcf1e19501da711a3e5fec2d
Kwasi Amanfi <Kamanfi@gmail.com>
2019-03-27 12:22:38 -0400

    fix type in this_cord.jsx preventing heroku deployment

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -11,7 +11,6 @@ export const usersReducer = (oldState = {}, action) => {
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
             return merge({},oldState,{[action.user.id]: action.user});
-            // debugger
         default:
             return oldState;
     }
--- frontend/this_cord.jsx
+++ frontend/this_cord.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import Root from './components/root';
+import Root from './components/Root';
+
+
 /////TESTING
 // import * as sessionUtil from './util/sessionApiUtil';
 import { login, logout, signup } from '../frontend/actions/session_actions';
