79aa3d6c973aa364e77e57a69229254bff5f8a3b
kwasi amanfi <kamanfi@gmail.com>
2019-04-05 10:34:22 -0400

    more css

--- frontend/components/chat/serverModal/InviteCodeModal.jsx
+++ frontend/components/chat/serverModal/InviteCodeModal.jsx
@@ -6,15 +6,18 @@ const InviteCodeModal = (props) => {
   
   return (
     <span className='invite_modal'>
-      <p>INVITE CODE</p>
+      <p>INVITE FRIENDS TO {props.server_name.toUpperCase()}</p>
+      <span className='spanclass'>Share this link with other to grant acess to this server</span>
       <input type="text" name="" id="" value={props.inviteCode}/>
+      <span>Your invite code will never expire</span>
     </span>
   )
 
 }
 const msp = (state, ownProps) => {
   return {
-    inviteCode: state.entities.currentServer.invite_code
+    inviteCode: state.entities.currentServer.invite_code,
+    server_name: state.entities.currentServer.server_name
   }
 }
 
